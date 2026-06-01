b29da1ce19ccb76dda9958ee5d09b5c294d9f669
SolemniMg <sgarcia@iespompeufabra.cat>
2026-06-01 08:32:02 +0200

    Create sketch.js

---
+++ snoopyfastiguejat/sketch.js
@@ -0,0 +1,403 @@
+function setup() {
+  createCanvas(800, 925);
+}
+
+function contornexterior(){
+  fill (255,255,255);
+  beginShape();//Contorn exterior
+vertex(514, 9.625);
+vertex(480, 23.625);
+vertex(444, 51.625);
+vertex(418, 79.625);
+vertex(382, 93.625);
+vertex(328, 103.625);
+vertex(286, 105.625);
+vertex(238, 115.625);
+vertex(208, 141.625);
+vertex(150, 205.625);
+vertex(134, 239.625);
+vertex(134, 269.625);
+vertex(134, 305.625);
+vertex(140, 343.625);
+vertex(174, 369.625);
+vertex(166, 363.625);
+vertex(194, 383.625);
+vertex(248, 401.625);
+vertex(300, 411.625);
+vertex(334, 419.625);
+vertex(384, 431.625);
+vertex(410, 447.625);
+vertex(416, 479.625);
+vertex(416, 509.625);
+vertex(394, 551.625);
+vertex(372, 591.625);
+vertex(352, 633.625);
+vertex(346, 677.625);
+vertex(358, 725.625);
+vertex(390, 765.625);
+vertex(420, 769.625);
+vertex(408, 803.625);
+vertex(344, 809.625);
+vertex(304, 811.625);
+vertex(288, 819.625);
+vertex(268, 825.625);
+vertex(248, 841.625);
+vertex(238, 865.625);
+vertex(246, 891.625);
+vertex(294, 893.625);
+vertex(360, 897.625);
+vertex(430, 909.625);
+vertex(486, 899.625);
+vertex(530, 899.625);
+vertex(558, 891.625);
+vertex(604, 881.625);
+vertex(590, 843.625);
+vertex(538, 825.625);
+vertex(508, 825.625);
+vertex(508, 775.625);
+vertex(530, 759.625);
+vertex(542, 717.625);
+vertex(540, 669.625);
+vertex(530, 637.625);
+vertex(506, 603.625);
+vertex(490, 555.625);
+vertex(476, 507.625);
+vertex(478, 465.625);
+vertex(508, 413.625);
+vertex(550, 387.625);
+vertex(576, 425.625);
+vertex(644, 411.625);
+vertex(680, 381.625);
+vertex(718, 323.625);
+vertex(726, 267.625);
+vertex(722, 187.625);
+vertex(704, 127.625);
+vertex(670, 73.625);
+vertex(616, 41.625);
+vertex(562, 7.625);
+endShape(CLOSE);}
+
+
+  
+function ull(){
+fill (36, 31, 33)
+beginShape();
+vertex(386.5, 180.609375);
+vertex(390.5, 191.609375);
+vertex(400.5, 196.609375);
+vertex(413.5, 196.609375);
+vertex(420.5, 190.609375);
+vertex(426.5, 179.609375);
+endShape();}
+
+  
+function nas(){
+fill (255,255,255);
+  beginShape();//Nas
+vertex(132.75, 270.1875);
+vertex(118.75, 267.1875);
+vertex(81.75, 292.1875);
+vertex(64.75, 313.1875);
+vertex(70.75, 345.1875);
+vertex(78.75, 355.1875);
+vertex(108.75, 358.1875);
+vertex(133.75, 340.1875);
+vertex(139.75, 333.1875);
+endShape(CLOSE);}
+  
+function cua(){
+  fill (255,255,255);
+  beginShape();//Cúa
+vertex(537.75, 739.6875);
+vertex(570.75, 739.6875);
+vertex(640.75, 757.6875);
+vertex(660.75, 776.6875);
+vertex(615.75, 796.6875);
+vertex(549.75, 770.6875);
+vertex(526.75, 757.6875);
+vertex(532.75, 740.6875);
+endShape(CLOSE);}
+  
+function pel(){
+beginShape();//Pel
+vertex(465.75, 36.1875);
+vertex(454.75, 43.1875);
+vertex(442.75, 25.1875);
+vertex(430.75, 10.1875);
+vertex(415.75, 21.1875);
+vertex(418.75, 7.1875);
+vertex(436.75, 0.1875);
+vertex(451.75, 9.1875);
+vertex(457.75, 25.1875);
+vertex(463.75, 33.1875);
+endShape(CLOSE);}
+  
+ function orella(){
+fill (255,255,255);
+  
+beginShape();//Orella
+vertex(585.75, 199.6875);
+vertex(561.75, 229.6875);
+vertex(544.75, 272.6875);
+vertex(538.75, 319.6875);
+vertex(546.75, 386.6875);
+endShape();}
+
+function peus(){
+fill (255,255,255);
+  beginShape();//Peus
+vertex(393.75, 880.6875);
+vertex(379.75, 880.6875);
+vertex(373.75, 896.6875);
+vertex(336.75, 893.6875);
+vertex(336.75, 871.6875);
+vertex(354.75, 868.6875);
+endShape();}
+  function peudret(){
+  fill (255,255,255);
+  
+ beginShape();//peudret
+vertex(465.75, 784.6875);
+vertex(456.75, 829.6875);
+vertex(438.75, 826.6875);
+vertex(405.75, 823.6875);
+vertex(390.75, 830.6875);
+vertex(355.75, 827.6875);
+vertex(343.75, 833.6875);
+vertex(310.75, 848.6875);
+vertex(294.75, 871.6875);
+vertex(303.75, 884.6875);
+vertex(306.75, 893.6875);
+endShape();}
+
+function peuesquerra(){
+fill (255,255,255);
+  beginShape();//peuesquerra
+vertex(277.75, 856.6875);
+vertex(265.75, 860.6875);
+vertex(267.75, 878.6875);
+vertex(267.75, 889.6875);
+endShape();}
+  
+function ma (){
+fill (255,255,255);
+  beginShape();//ma
+vertex(459.75, 622.6875);
+vertex(454.75, 649.6875);
+vertex(451.75, 695.6875);
+vertex(453.75, 731.6875);
+vertex(462.75, 757.6875);
+vertex(490.75, 767.6875);
+vertex(519.75, 761.6875);
+vertex(529.75, 754.6875);
+endShape();}
+  
+function dits(){
+
+  beginShape();//dits
+vertex(496.75, 733.6875);
+vertex(493.75, 755.6875);
+vertex(493.75, 764.6875);
+endShape();}
+  
+function dits(){
+  beginShape();//dits
+vertex(511.75, 724.6875);
+vertex(517.75, 731.6875);
+vertex(529.75, 734.6875);
+vertex(537.75, 734.6875);
+endShape();}
+  
+  function collar(){
+ fill (33,30,32);
+ beginShape();//collar
+vertex(402.75, 497.6875);
+vertex(417.75, 485.6875);
+vertex(444.75, 481.6875);
+vertex(471.75, 478.6875);
+vertex(486.75, 478.6875);
+vertex(492.75, 484.6875);
+vertex(481.75, 488.6875);
+vertex(466.75, 499.6875);
+vertex(454.75, 506.6875);
+vertex(435.75, 512.6875);
+vertex(417.75, 508.6875);
+vertex(400.75, 499.6875);
+endShape();}
+    
+function brilldelcollar(){
+  
+fill (255,255,255);
+  beginShape();//brilldelcollar
+vertex(461.75, 488.6875);
+vertex(452.75, 489.6875);
+vertex(447.75, 493.6875);
+vertex(459.75, 496.6875);
+vertex(464.75, 492.6875);
+endShape(CLOSE);}
+  
+  function bocaenfadada(){
+  beginShape();//bocaenfadada
+vertex(472.5, 315.609375);
+vertex(449.5, 306.609375);
+vertex(393.5, 353.609375);
+vertex(381.5, 363.609375);
+vertex(380.5, 376.609375);
+vertex(369.5, 394.609375);
+vertex(355.5, 410.609375);
+vertex(346.5, 418.609375);
+vertex(337.5, 423.609375);
+    
+endShape();}
+  
+function manchaorella(){
+fill(35,30,32);
+  beginShape();//manchaorella
+vertex(648.75, 157.6875);
+vertex(633.75, 175.6875);
+vertex(625.75, 173.6875);
+vertex(618.75, 180.1875);
+vertex(586.75, 239.1875);
+vertex(572.75, 264.1875);
+vertex(572.75, 285.1875);
+vertex(567.75, 292.1875);
+vertex(568.75, 317.1875);
+vertex(567.75, 334.1875);
+vertex(571.75, 354.1875);
+vertex(573.75, 368.1875);
+vertex(573.75, 379.1875);
+vertex(584.75, 396.1875);
+vertex(597.75, 403.1875);
+vertex(605.75, 399.1875);
+vertex(613.75, 402.1875);
+vertex(627.75, 389.1875);
+vertex(647.75, 383.1875);
+vertex(676.75, 338.1875);
+vertex(692.75, 307.1875);
+vertex(701.75, 279.1875);
+vertex(701.75, 245.1875);
+vertex(695.75, 231.1875);
+vertex(694.75, 203.6875);
+vertex(689.75, 191.6875);
+vertex(684.75, 191.6875);
+vertex(682.75, 178.6875);
+vertex(675.75, 171.6875);
+vertex(666.75, 162.6875);
+vertex(654.75, 188.6875);
+vertex(646.75, 191.6875);
+vertex(631.75, 202.6875);
+vertex(643.75, 181.6875);
+vertex(657.75, 161.6875);
+vertex(651.75, 155.6875);
+endShape();}
+  
+  function manchadedinsdelaorella1(){
+  fill (255,255,255);
+ beginShape();//manchadedinsdelaorella1
+vertex(669.75, 208.1875);
+vertex(666.75, 215.1875);
+vertex(662.75, 227.1875);
+vertex(661.75, 236.1875);
+vertex(658.75, 245.1875);
+vertex(656.75, 253.1875);
+vertex(659.75, 254.1875);
+vertex(663.75, 250.1875);
+vertex(663.75, 240.1875);
+vertex(666.75, 230.1875);
+vertex(668.75, 220.1875);
+endShape(CLOSE);}
+  function manchadedinsdelaorella2(){
+    fill (255,255,255);
+
+beginShape();//Mancha de dins de la orella 2
+vertex(629.75, 226.1875);
+vertex(624.75, 234.1875);
+vertex(616.75, 234.1875);
+vertex(615.75, 248.1875);
+vertex(615.75, 255.1875);
+vertex(610.75, 261.1875);
+vertex(606.75, 273.1875);
+vertex(605.75, 286.1875);
+vertex(614.75, 278.1875);
+vertex(614.75, 268.1875);
+vertex(620.75, 253.1875);
+vertex(631.75, 229.1875);
+endShape(CLOSE);}
+  function manchadedinsdelaorella3(){
+    fill (255,255,255);
+
+  beginShape();//manchadedinsdelaorella 3
+vertex(660.75, 285.6875);
+vertex(657.75, 289.6875);
+vertex(660.75, 295.6875);
+vertex(665.75, 293.6875);
+vertex(665.75, 286.6875);
+endShape(CLOSE);}
+  
+  function manchaesquena(){
+    fill ((36,31,33));
+  
+  beginShape();//manchaesquena
+vertex(526.75, 638.1875);
+vertex(520.75, 648.1875);
+vertex(515.75, 658.1875);
+vertex(513.75, 672.1875);
+vertex(515.75, 692.1875);
+vertex(513.75, 700.1875);
+vertex(528.75, 711.1875);
+vertex(535.75, 714.1875);
+vertex(540.75, 714.6875);
+vertex(529.75, 635.6875);
+endShape();}
+
+function cella(){
+beginShape();//cella
+vertex(365.5, 185.609375);
+vertex(386.5, 180.609375);
+vertex(410.5, 179.609375);
+vertex(429.5, 179.609375);
+vertex(446.5, 179.609375);
+endShape();}
+
+
+  
+  function manchanas(){
+  fill ((36,31,33));
+  beginShape();//manchanas
+curveVertex(132.75, 307.1875);
+curveVertex(115.75, 307.1875);
+curveVertex(96.75, 318.1875);
+curveVertex(88.75, 331.1875);
+curveVertex(91.75, 343.1875);
+curveVertex(102.75, 357.1875);
+endShape(CLOSE);}
+
+  function draw(){//
+  let h=sin(frameCount*0.05)*5;
+  contornexterior();
+    ull();
+    nas();
+    cua();
+    pel();
+    orella();
+    peus();
+    peudret();
+    peuesquerra();
+    ma();
+    dits();
+    dits();
+    collar();
+    brilldelcollar();
+     bocaenfadada();
+    manchaorella();
+    manchadedinsdelaorella1();
+    manchadedinsdelaorella2();
+    manchadedinsdelaorella3();
+    manchaesquena();
+    cella();
+  
+    manchanas();
+  }
+    
+
+    
